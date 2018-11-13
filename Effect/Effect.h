1bbbc759e76a5ac9091d44a8dc7ed28a68297453
tokijh <tokijh@naver.com>
2018-11-13 23:31:04 +0900

    Initial Commit

---
+++ Effect/Effect.h
@@ -0,0 +1,19 @@
+//
+//  Effect.h
+//  Effect
+//
+//  Created by 윤중현 on 13/11/2018.
+//  Copyright © 2018 tokijh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Effect.
+FOUNDATION_EXPORT double EffectVersionNumber;
+
+//! Project version string for Effect.
+FOUNDATION_EXPORT const unsigned char EffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Effect/PublicHeader.h>
+
+
